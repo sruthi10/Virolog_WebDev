f2d7c3f30fbdf72ab6a5ff026032e17b4bd85f4c
Cerbes <cte150030@utdallas.edu>
2020-06-11 16:11:07 -0500

    Document main file

--- main.py
+++ main.py
@@ -1,19 +1,80 @@
+"""
+Flask App Server
+
+This file creates a Flask app and registers different routes for the Flask web server. Running this file will start the web server.
+
+The following classes and functions are available:
+    * FloatConverter - class required to pass floats in a URL route
+    * dashboard - renders view for home page dashboard
+    * cleavageSitesView - renders view for cleavage site page
+    * getCleavageSitesData - get ceavage site data with min P-value
+    * presequenceProbabilityView - renders view for preseq prob page
+    * getPresequenceProbabilityData - get preseq prob data given P-value range
+    * virusTaxonomyView - renders view for viral tax page
+    * getVirusTaxonomyData - get all viral tax data
+    * getFilteredTaxonomyData - get viral tax data based on list
+"""
+
 from flask import Flask, render_template, jsonify, request
 from genedata import cleavage_sites, presequence_probability, virus_taxonomy
 from werkzeug.routing import FloatConverter as BaseFloatConverter
-import sys # only need to printing to console
+import sys # only need for printing to console
 
 app = Flask(__name__)
 
+
 class FloatConverter(BaseFloatConverter):
+    """
+    A class used to convert to float
+    
+    Attributes
+    ----------
+    regex : str
+        a formatted regex string to represent a float
+    """
     regex = r'-?\d+(\.\d+)?'
 
-# before routes are registered
+""" NOTE: must define this before routes are registered"""
 app.url_map.converters['float'] = FloatConverter
 
 
+@app.route('/')
+def dashboard():
+    """Renders view for home page dashboard
+
+    Returns
+    -------
+    string
+        a string representing the evaluated html dashboard template
+    """
+    return render_template('dashboard.html')
+
+
+@app.route('/cleavagesites')
+def cleavageSitesView():
+    """Renders view for cleavage sites page
+
+    Returns
+    -------
+    string
+        a string representing the evaluated cleavage sites template
+    """
+    return render_template('partials/cleavagesites.html')
+
 @app.route('/getcleavagesitesdata/<float:pvalue>')
 def getCleavageSitesData(pvalue):
+    """Fetches data to be displayed on the cleavage sites page
+    
+    Parameters
+    ----------
+    pvalue : float
+        The min P-value for the data returned
+
+    Returns
+    -------
+    flask.Response()
+        an object with content-type header 'application/json' that contains cleavage site data
+    """
     cleavage_mapping_virus = cleavage_sites.get_mapping(
         r'genedata/outputdata/mitofates_viral_cleaned.csv', pvalue)
     cleavage_mapping_human = cleavage_sites.get_mapping(
@@ -39,8 +100,33 @@ def getCleavageSitesData(pvalue):
     return jsonify(jsonData)
 
 
+@app.route('/presequenceprobability')
+def presequenceProbabilityView():
+    """Renders view for presequence probability page
+
+    Returns
+    -------
+    string
+        a string representing the evaluated preseq prob template
+    """
+    return render_template('partials/presequenceprobability.html')
+
 @app.route('/getPresequenceProbabilityData/<float:pvalueMin>/<float:pvalueMax>')
 def getPresequenceProbabilityData(pvalueMin, pvalueMax):
+    """Fetches data to be displayed on the preseq prob page
+    
+    Parameters
+    ----------
+    pvalueMin : float
+        The min P-value for the data returned
+    pvalueMax : float
+        The max P-value for the data returned
+
+    Returns
+    -------
+    flask.Response()
+        an object with content-type header 'application/json' that contains preseq prob data
+    """
     probability_mapping_virus = presequence_probability.get_mapping(
         r'genedata/outputdata/mitofates_viral_cleaned.csv')
     probability_mapping_virus_mapping_human = presequence_probability.get_mapping(
@@ -66,43 +152,46 @@ def getPresequenceProbabilityData(pvalueMin, pvalueMax):
     return jsonify(jsonData)
 
 
-@app.route('/cleavagesites')
-def cleavageSitesView():
-    return render_template('partials/cleavagesites.html')
+@app.route('/virusTaxonomy')
+def virusTaxonomyView():
+    """Renders view for viral taxonomy page
 
-
-@app.route('/presequenceprobability')
-def presequenceProbabilityView():
-    return render_template('partials/presequenceprobability.html')
-
+    Returns
+    -------
+    string
+        a string representing the evaluated html viral taxonomy template
+    """
+    return render_template('partials/viraltaxonomy.html')
 
 @app.route('/getVirusTaxonomyData')
 def getVirusTaxonomyData():
+    """Fetches all the viral tax data to be displayed
+
+    Returns
+    -------
+    flask.Response()
+        an object with content-type header 'application/json' that contains viral tax labels and data
+    """
     virus_taxonomy_distribution = virus_taxonomy.getTaxonomyDistribution()
     label, values = virus_taxonomy.format_taxonomy(virus_taxonomy_distribution)
     return jsonify({"labels": label, "data": values})
-	
-@app.route('/getAllVirusTaxonomyData')
-def getAllVirusTaxonomyData():
-    return jsonify(virus_taxonomy.getTaxonomyData())
-
-@app.route('/getFilteredVirusTaxonomyData/<filteredList>')
-def getFilteredVirusTaxonomyData(filteredList):
-    return jsonify(virus_taxonomy.getTaxonomyData(filteredList))
 
 @app.route('/filteredTaxonomyData/<filteredList>')
 def getFilteredTaxonomyData(filteredList):
+    """Fetches all the viral tax data to be displayed
+    
+    Parameters
+    ----------
+    filteredList : list
+        list of the labels to not include for the data/ visualizations
+
+    Returns
+    -------
+    flask.Response()
+        an object with content-type header 'application/json' that contains viral tax labels and data, not including the labels listed in filteredList
+    """
     print(request.args, file=sys.stderr)
     return jsonify(virus_taxonomy.newGetTaxonomyData(request.args, filteredList))
-
-@app.route('/virusTaxonomy')
-def virusTaxonomyView():
-    return render_template('partials/viraltaxonomy.html')
-
-
-@app.route('/')
-def dashboard():
-    return render_template('dashboard.html')
 
 
 if __name__ == '__main__':
