5966f5d27aae89d4061d6473098ded4f1bb1e0bc
Cerbes <cte150030@utdallas.edu>
2020-08-17 13:38:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -195,4 +195,4 @@ def getFilteredTaxonomyData(filteredList):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True, use_reloader=True) """For cloud deployment, add param host='0.0.0.0'"""
